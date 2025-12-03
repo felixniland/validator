ea7aee8147360a22e29f8189a2b199e4da97777b
felixniland <felixniland@gmail.com>
2025-12-04 09:19:57 +1100

    tightened up isAlpha

--- src/lib/kb/index.ts
+++ src/lib/kb/index.ts
@@ -33,7 +33,17 @@ const isCloseBracket = newStrValidator(CLOSE_BRACKET);
 
 // regex
 const isNumKey = (str: string): str is NumKey => str.length === 1 && /^[0-9]$/.test(str);
-const isAlpha = (str: string): str is Alpha => str.length === 1 && /^[a-zA-Z]$/.test(str);
+// this old version of "isAlpha" only works with English // const isAlpha = (str: string): str is Alpha => str.length === 1 && /^[a-zA-Z]$/.test(str);
+
+/**
+ * @param str a string of any length
+ * @returns boolean representing if the string is an alpha
+ * NOTE:
+ * - the "Alpha" type used is only the English alphabet in both upper & lowercase;
+ * - however, this will actually return for all alphabet characters (i.e., Greek, Russian...)
+ * - internally, it compares (for each char of the string) "does uppercase === lowercase", thus returning FALSE for numbers, emojis, etc
+ */
+const isAlpha = (str: string): str is Alpha => [...str].every((s) => s.toUpperCase() !== s.toLowerCase());
 
 const isInputKey = (v: unknown): v is KBTextInput => isStr(v) && (isAlpha(v) || isNumKey(v) || isSpecialChar(v));
 const isNonInputKey = (v: unknown): v is KBNonTextInput => isStr(v) && (isFunctionKey(v) || isEditingKey(v) || isNavKey(v));
