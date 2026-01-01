79312ddbe43273deb082c0c35d86fde76f5195b1
felixniland <felixniland@gmail.com>
2026-01-01 12:08:30 +1100

    removed the ReturnType from newStrValidator, as it was displaying in intellisense as 'StrValidator<'someStrLiteral'>', rather than 'v is someStrLiteral'; also commented-out the generateStrValidatorWithPRops fn, as it is a silly idea for a validator...!

--- src/lib/str/index.ts
+++ src/lib/str/index.ts
@@ -8,7 +8,7 @@ import type { NonEmptyArr } from "$lib/internal/types.js";
  */
 
 export {
-    newStrValidatorWithProps as newStrValidator,
+    newStrValidator
 }
 
 export type {
@@ -24,7 +24,7 @@ type StrValidatorOpts = {
 };
 
 /** i.e., without props lol */
-function newStrValidator<T extends string>(arr: NonEmptyArr<T>): StrValidatorFn<T> {
+function newStrValidator<T extends string>(arr: NonEmptyArr<T>) {
     const set = new Set(arr);
     
     const fn = (val: unknown): val is T => (
@@ -34,23 +34,24 @@ function newStrValidator<T extends string>(arr: NonEmptyArr<T>): StrValidatorFn<
     return fn;
 }
 
+// NTS: stopped using this, as the params make the "v is T" return a lie. this was a silly idea for a validator; it makes more sense as a util, esp if it returns the transformed match (e.g., lowercase'd) 
 // generateStrValidator<T extends string>(arr: Array<T>, opts?: StrValidatorOpts<true>): string;
 // generateStrValidator<T extends string>(arr: Array<T>, opts?: StrValidatorOpts<false>): string;
-function newStrValidatorWithProps<T extends string>(arr: NonEmptyArr<T>, opts?: StrValidatorOpts): StrValidatorFn<T> {
-    const normalised = opts?.caseInsensitive ? arr.map(str => str.toLowerCase()) : arr;
+// function newStrValidatorWithProps<T extends string>(arr: NonEmptyArr<T>, opts?: StrValidatorOpts): StrValidatorFn<T> {
+//     const normalised = opts?.caseInsensitive ? arr.map(str => str.toLowerCase()) : arr;
     
-    const set = new Set(normalised);
-    const normalisedStr = (str: string): string => opts?.caseInsensitive ? str.toLowerCase() : str;
+//     const set = new Set(normalised);
+//     const normalisedStr = (str: string): string => opts?.caseInsensitive ? str.toLowerCase() : str;
 
-    // const passMin = (typeof opts?.min === "number") ? (str: string) => min(str, opts.min!) : true;
-    // const passMax = (typeof opts?.max === "number") ? (str: string) => min(str, opts.max!) : true;
+//     // const passMin = (typeof opts?.min === "number") ? (str: string) => min(str, opts.min!) : true;
+//     // const passMax = (typeof opts?.max === "number") ? (str: string) => min(str, opts.max!) : true;
     
-    const fn = (val: unknown): val is T => (
-        isStr(val)
-        && set.has(normalisedStr(val)) 
-        // && (typeof passMin === "boolean" ? passMin : passMin(val))
-        // && (typeof passMax === "boolean" ? passMax : passMax(val))
-    );
+//     const fn = (val: unknown): val is T => (
+//         isStr(val)
+//         && set.has(normalisedStr(val)) 
+//         // && (typeof passMin === "boolean" ? passMin : passMin(val))
+//         // && (typeof passMax === "boolean" ? passMax : passMax(val))
+//     );
 
-    return fn;
-}
+//     return fn;
+// }
