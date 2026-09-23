053b265d06f53a632e46f58cfa4d297b8473a97e
Felix Niland <felixniland@gmail.com>
2026-09-23 19:22:56 +1000

    whoops, buggered up a ReturnType for 'isAsyncFn'. fixed

--- src/lib/is/isAsyncFn.ts
+++ src/lib/is/isAsyncFn.ts
@@ -1,6 +1,4 @@
-import type { FnInOut } from "felixtypes";
-
 const ASYNC_FUNCTION = Object.getPrototypeOf(async function(){}).constructor;
 
 /** Checks if val is an async function */
-export const isAsyncFn = (v: unknown): v is FnInOut<unknown, unknown, "async"> => v instanceof ASYNC_FUNCTION;
+export const isAsyncFn = (v: unknown): v is (...args: any) => Promise<any> => v instanceof ASYNC_FUNCTION;
